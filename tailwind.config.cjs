5804dddef98ba2f240558bcb49de267e7d210dd8
Zachariah Ngonyani <zachariahn@gmail.com>
2024-04-24 20:35:12 +0300

    feat: add roboto font as default

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,25 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        sans: [
+          "Roboto",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
       primary: "#202225",
       secondary: "#5865f2",
       gray: colors.neutral,
