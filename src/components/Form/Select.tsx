f5ba7df87f552c8f2cbab8ab887981628ff8f344
Zachariah Ngonyani <zachariahn@gmail.com>
2024-04-27 13:30:23 +0300

    feat: add select component

--- src/components/Form/Select.tsx
+++ src/components/Form/Select.tsx
@@ -1,26 +1,47 @@
+import React, { useState } from "react";
+
+const options = [
+  "1-10 Employees",
+  "11-50 Employees",
+  "51-200 Employees",
+  "201-500 Employees",
+  "501-1000 Employees",
+  "1000+ Employees",
+];
+
 const Select = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
   return (
     <div>
       <div className="relative">
         <select
           name="company-size"
           id="company-size"
+          defaultValue=""
+          className="pl-3 peer placeholder-transparent"
           placeholder={props.label}
-          className="pl-2 peer placeholder-transparent"
         >
-          <option value="1-10">1-10 Employees</option>
-          <option value="11-50">11-50 Employees</option>
-          <option value="51-200">51-200 Employees</option>
-          <option value="201-500">201-500 Employees</option>
-          <option value="501-1000">501-1000 Employees</option>
-          <option value="1000+">1000+ Employees</option>
+          {options.map((opt, idx) => (
+            <option key={idx} value={opt}>
+              {opt}
+            </option>
+          ))}
         </select>
         <label
           htmlFor={props.id}
           className="
           absolute
           left-2
-          -top-2
+          -top-2.5
           px-1
           text-gray-600 
           text-xs
@@ -28,9 +49,9 @@ const Select = (props) => {
           transition-all
           peer-placeholder-shown:text-base
           peer-placeholder-shown:text-gray-400
-          peer-placeholder-shown:top-2
+          peer-placeholder-shown:top-2.5
           peer-placeholder-shown:left-2
-          peer-focus:-top-2
+          peer-focus:-top-2.5
           peer-focus:text-gray-600
           peer-focus:text-xs
           peer-focus:left-2
