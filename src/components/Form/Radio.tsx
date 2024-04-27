c1ec480074b49f619248be581ceadd479921dd43
Zachariah Ngonyani <zachariahn@gmail.com>
2024-04-27 13:30:31 +0300

    feat: create radio button

--- src/components/Form/Radio.tsx
+++ src/components/Form/Radio.tsx
@@ -1,7 +1,12 @@
 const Radio = (props) => {
   return (
     <div className="flex items-center">
-      <input id={props.id} name="terms-and-privacy" type="radio" className="" />
+      <input
+        id={props.id}
+        name="terms-and-privacy"
+        type="radio"
+        className="border border-gray-400"
+      />
       <label htmlFor={props.id} className="ml-2 block text-sm text-gray-900">
         {props.label}
       </label>
