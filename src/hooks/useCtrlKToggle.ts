855e6084320fc0b539038cb846cef915b28560ef
Zachariah Ngonyani <zachariahn@gmail.com>
2024-04-29 13:15:23 +0300

    feat: create hooks for use ctrl + k to open command palette

--- src/hooks/useCtrlKToggle.ts
+++ src/hooks/useCtrlKToggle.ts
@@ -1,19 +1,26 @@
 import { useEffect } from "react";
 
-function useCtrlKToggle(setIsOpen, isOpen) {
+// Define the hook as an arrow function with type annotations for the parameters.
+const useCtrlKToggle = (
+  setIsOpen: (isOpen: boolean) => void,
+  isOpen: boolean,
+): void => {
   useEffect(() => {
-    function onKeydown(event) {
+    // Event handler for keydown that toggles open state based on 'Ctrl+K' or 'Cmd+K'.
+    const onKeydown = (event: KeyboardEvent): void => {
       if (event.key === "k" && (event.metaKey || event.ctrlKey)) {
-        event.preventDefault();
-        setIsOpen(!isOpen); //
+        event.preventDefault(); // Prevent the default action of the key event.
+        setIsOpen(!isOpen); // Toggle the boolean state based on previous state.
       }
-    }
+    };
 
+    // Add the event listener when the component mounts.
     window.addEventListener("keydown", onKeydown);
+    // Cleanup function to remove the event listener when the component unmounts.
     return () => {
       window.removeEventListener("keydown", onKeydown);
     };
-  }, [isOpen, setIsOpen]);
-}
+  }, [isOpen, setIsOpen]); // Dependencies array to re-run the effect when dependencies change.
+};
 
 export default useCtrlKToggle;
